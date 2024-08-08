1dab8e0368c78f34ca57fddaa837c86fc7f54a3a
Defiant7426 <pldelacruzv@gmail.com>
2024-08-08 02:14:11 -0500

    Agregando head

---
+++ detector-spam/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="py-5 header">
+
+        <a href="index.html">
+          <img
+            className="img-fluid header-spam"
+            src="./public/Detector_Spam.gif"
+            alt="imagen logo"
+          />
+        </a>
+
+    </header>
+  )
+}
